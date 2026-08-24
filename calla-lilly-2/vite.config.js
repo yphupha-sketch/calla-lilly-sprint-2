5924f3d32feb28f9276555f272c81abe0579923d
phupha <y.phupha@gmail.com>
2026-08-24 15:18:42 +0700

    1st-commit

---
+++ calla-lilly-2/vite.config.js
@@ -0,0 +1,13 @@
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
+  ],
+})
