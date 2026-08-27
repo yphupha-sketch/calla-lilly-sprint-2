6264ff3e8d40dc1ed96de1d3c6d9a40ad367c582
rewatbu <rewatbu@gmail.com>
2026-08-27 10:34:46 +0700

    styling: update App.css and App.jsx

--- calla-lilly-2/src/App.jsx
+++ calla-lilly-2/src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 import './App.css'
 
+import ProductCard from "./components/ProductCard"
+import CartItem from "./components/CartItem"
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Calla Lily Sprint 2</h1>
+      <br />
+      <h2>This is a current ProductCard component.</h2>
+      <ProductCard name="Calla Lily Premium Soap 1" price={120} />
+      <ProductCard name="Calla Lily Premium Soap 2" price={120} />
+      <ProductCard name="Calla Lily Premium Soap 3" price={120} />
+      <br />
+      <h2>This is a current CartItem component.</h2>
+      <CartItem name="Calla Lily Premium Soap 1" price={120} />
+      <CartItem name="Calla Lily Premium Soap 2" price={120} />
+      <CartItem name="Calla Lily Premium Soap 3" price={120} />
+
     </>
   )
 }
