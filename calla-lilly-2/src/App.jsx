88307987cc5884837cdd68b6a1bb0327d0262fcf
rewatbu <rewatbu@gmail.com>
2026-08-27 11:02:08 +0700

    update: App.jsx for rendering components

--- calla-lilly-2/src/App.jsx
+++ calla-lilly-2/src/App.jsx
@@ -3,24 +3,28 @@ import './App.css'
 
 import ProductCard from "./components/ProductCard"
 import CartItem from "./components/CartItem"
+import Navbar from "./components/Navbar"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Calla Lily Sprint 2</h1>
-      <br />
-      <h2>This is a current ProductCard component.</h2>
-      <ProductCard name="Calla Lily Premium Soap 1" price={120} />
-      <ProductCard name="Calla Lily Premium Soap 2" price={120} />
-      <ProductCard name="Calla Lily Premium Soap 3" price={120} />
-      <br />
-      <h2>This is a current CartItem component.</h2>
-      <CartItem name="Calla Lily Premium Soap 1" price={120} />
-      <CartItem name="Calla Lily Premium Soap 2" price={120} />
-      <CartItem name="Calla Lily Premium Soap 3" price={120} />
+      <Navbar cartCount={3} />
 
+      <main>
+        <h1>Calla Lily Sprint 2</h1>
+        <br />
+        <h2>This is a current ProductCard component.</h2>
+        <ProductCard name="Calla Lily Premium Soap 1" price={120} />
+        <ProductCard name="Calla Lily Premium Soap 2" price={120} />
+        <ProductCard name="Calla Lily Premium Soap 3" price={120} />
+        <br />
+        <h2>This is a current CartItem component.</h2>
+        <CartItem name="Calla Lily Premium Soap 1" price={120} />
+        <CartItem name="Calla Lily Premium Soap 2" price={120} />
+        <CartItem name="Calla Lily Premium Soap 3" price={120} />
+      </main>
     </>
   )
 }
