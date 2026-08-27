2df3c8f385484104fb262b821c470ba4d1a8e0ea
rewatbu <rewatbu@gmail.com>
2026-08-27 10:02:50 +0700

    create component folder and add Button, CartItem, and ProductCard components

--- calla-lilly-2/src/App.jsx
+++ calla-lilly-2/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
