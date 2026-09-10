6744c768686914e934046d5e5f3ecd4dc2d39254
rewatbu <rewatbu@gmail.com>
2026-09-10 22:00:02 +0700

    save orderController.js

---
+++ backend/src/config/controllers/orderController.js
@@ -0,0 +1,8 @@
+export const createOrder = async (req, res) => {
+  // 1. Get checkout data
+  // 2. Validate products
+  // 3. Calculate prices
+  // 4. Create Order
+  // 5. Save to MongoDB
+  // 6. Return created order
+};
