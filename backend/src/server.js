d1af3d70ca920466a78d21792bda50732c6b8685
rewatbu <rewatbu@gmail.com>
2026-09-15 10:39:59 +0700

    add cors to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import connectDB from './config/db.js';
+import productsRoute from './routes/productsRoute.js';
 
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
+
+app.use('/api/products', productsRoute);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Calla Lilly API is running' });
