c460ce4459cd5cd1149b86603a43155b2225dc71
rewatbu <rewatbu@gmail.com>
2026-09-15 11:48:50 +0700

    rename file

--- frontend/src/context/ProductContext.jsx
+++ frontend/src/context/ProductContext.jsx
@@ -7,7 +7,7 @@
 // ============================================================
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useEffect, useState } from "react";
-import seedProducts from "../data/product";
+import seedProducts from "../data/products";
 
 const PRODUCTS_KEY = "calla-products";
 
